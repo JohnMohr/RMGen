8853e1b7f87c7734b6b769e3ce8889f859ac3d82
JohnMohr <74803311+JohnMohr@users.noreply.github.com>
2021-01-09 11:55:59 -0800

    questions written

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require("./utils/generateMarkdown.js")
 
-// TODO: Create an array of questions for user input
-const questions = [];
+//  Array of questions for user input
+const questions = [{
+    type:'input' ,
+    message: 'What is your GitHub user name?',
+    name: 'userName',
+},
+    {
+    type: 'input',
+    message:'What is the name of your GitHub Repo?' ,
+    name: 'userRepo',
+},
+    {
+    type: 'input',
+    message:'What is your project title?' , 
+    name: 'projectTitle',
+},
+    {
+    type: 'input',
+    message: 'Describe your project:',
+    name: 'projectStory',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
