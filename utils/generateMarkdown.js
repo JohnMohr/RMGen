e98c4215e40e08ad788501c7ff57e69f6d49de54
JohnMohr <74803311+JohnMohr@users.noreply.github.com>
2021-01-10 12:33:24 -0800

    added license badge. about section.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,16 +1,16 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// // TODO: Create a function that returns a license badge based on which license is passed in
+// // If there is no license, return an empty string
+// function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) {}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection(license) {}
 
-// TODO: Create a function to generate markdown for README
+// Function to generate markdown for README
 function generateMarkdown(userResponses, userInfo) {
     //Create ToC based on Questions' userResponses.
 let draftToC = `## Table of Conents`;
@@ -29,14 +29,68 @@ if (userResponses.projectContrib !== '') {
  let draftMarkdown =  
  `# ${userResponses.projectTitle}
 
-![Github Repo Top Language](https://img.shields.io/github/languages/top/${userResponses.userName}/${userResponses.userRepo}?style=flat&logo=appveyor) ![License Badge]('')
+![Github Repo Top Language](https://img.shields.io/github/languages/top/${userResponses.userName}/${userResponses.userRepo}?style=flat&logo=appveyor) ![License Badge]('https://img.shields.io/badge/License-${userResponses.license}-brightgreen.svg')
 
 ## Description
 
-*This is the how, what, when, where, why.*
+*This is the how, what, when, where, and why.*
 
+${userResponses.projectStory}`
 
+//Add ToC to markdown document
+draftMarkdown += draftToc;
+
+//add license
+draftMarkdown += `* [License](#projectLicense)`;
+
+//optional sections dependant on provided answer existing
+if(userResponses.projectInstall !== '') {
+    draftMarkdown += `
+## Installation
+
+*Required Steps of Installation:*
+${userResponses.projectInstall}
+`};
+if(userResponses.projectInstall !== '') {
+    draftMarkdown += `
+## Usage
+    
+*Instructions and Usage:*
+${userResponses.projectStory}
+`};
+if(userResponses.projectStory !== '') {
+    draftMarkdown += `
+## Contributing
+    
+*People, Places & Things :*
+    ${userResponses.projectContrib}
+`};
+//License section
+draftMarkdown += `
+
+## License
+
+${userResponses.license}
 `;
+
+// About Section
+let draftAbout =`
+***
+***
+
+##Questions?
+
+![Dev Profile](${userInfo.avatar_url})
+
+Questions? Concerns? Good joke? Get at me:
+
+GitHub: [@${userInfo.login}](${userInfo.url})
+
+`
+//add About section
+draftMarkdown += draftAbout;
+//KOBE
+return draftMarkdown;
 }
 
 module.exports = generateMarkdown;
